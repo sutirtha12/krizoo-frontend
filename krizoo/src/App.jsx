7ea99be632713c508e4e38c6d05df483e151141d
sutirtha saha <sutirtha@sutirthas-MacBook-Air.local>
2025-12-31 06:05:45 +0530

    Added logo

--- krizoo/src/App.jsx
+++ krizoo/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useState , useEffect} from "react";
 
 /* PUBLIC COMPONENTS */
 import Navbar from "./components/Navbar";
